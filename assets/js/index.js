994d4e9314166699e968d3129c46d85b4d0de7cb
S4nt1nO <Gianluca1996@gmx.de>
2023-07-06 14:07:16 +0200

    index.html done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
 'use strict'
+
+/*
+#Heute werden wir einen Kalorienrechner programmieren. 
+#Zuerst müssen wir den Grundumsatz berechnen. Der Grundumsatz wird anhand der Harris Benedict Formel berechnet.
+#Grundumsatz bei Männern (Kalorien je Tag)
+#66.47 + (13.7 * Körpergewicht in kg) + (5 * Körpergröße in cm) – (6.8 * Alter in Jahren) = Grundumsatz 
+#Grundumsatz bei Frauen (Kalorien je Tag)
+#655.1 + (9.6 * Körpergewicht in kg) + (1.8 * Körpergröße in cm) – (4.7 * Alter in Jahren) = Grundumsatz
+? Das Ergebnis wird in Kilokalorien ausgegeben.
+
+=====================================================
+- index.html
+- constanten ziehen
+- dropdownlist ziehen und werte definieren
+- gedanken ueber events machen
+- inputfields verbinden mit radio buttons um mit der value vom input arbeiten zu koennen ifelse
+- umsaetze mit let variablen berechnen ( neu definieren in ifelse )
+? 1cal = 4184j
+*/
